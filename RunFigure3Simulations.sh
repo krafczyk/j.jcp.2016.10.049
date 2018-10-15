18dd4654e2fad0704b04cd74813ba5615ef66f20
Matthew Krafczyk <krafczyk.matthew@gmail.com>
2018-10-15 10:50:10 -0500

    Update notes and script

--- RunFigure3Simulations.sh
+++ RunFigure3Simulations.sh
@@ -1,3 +1,3 @@
 echo "Left Figures"
-echo "\"Lattice Size\", \"NY\", \"Gamma\", \"Tau\", \"Error\"" > Figure3_nonconvex.dat
-parallel -j4 --colsep '\t' ./Poiseuille/poiseuille_nonconvex --gamma {1} --tau {2} --Ny {3} >> Figure3_nonconvex.dat :::: Figure3_nonconvex_pars.dat
+echo "\"Lattice Size\", \"NY\", \"Gamma\", \"Tau\", \"Error\"" > Figure3_nonconvex.out
+parallel -j4 --colsep '\t' ./Poiseuille/poiseuille_nonconvex --gamma {1} --tau {2} --Ny {3} >> Figure3_nonconvex.out :::: Figure3_nonconvex.pars
