030599b017ed05ab86ab500c9a3ea9beba7e8e01
Matthew Krafczyk <krafczyk.matthew@gmail.com>
2018-10-15 13:55:51 -0500

    Add convex simulation parameters

--- RunFigure3Simulations.sh
+++ RunFigure3Simulations.sh
@@ -1,3 +1,11 @@
 echo "Left Figures"
-echo "\"Lattice Size\", \"NY\", \"Gamma\", \"Tau\", \"Error\"" > Figure3_nonconvex.out
-parallel -j4 --colsep '\t' ./Poiseuille/poiseuille_nonconvex --gamma {1} --tau {2} --Ny {3} >> Figure3_nonconvex.out :::: Figure3_nonconvex.pars
+if [ ! -e "Figure3_nonconvex.out" ]; then
+	echo "\"Lattice Size\", \"NY\", \"Gamma\", \"Tau\", \"Error\"" > Figure3_nonconvex.out
+	parallel -j4 --colsep '\t' ./Poiseuille/poiseuille_nonconvex --gamma {1} --tau {2} --Ny {3} >> Figure3_nonconvex.out :::: Figure3_nonconvex.pars
+fi;
+
+echo "Right Figures"
+if [ ! -e "Figure3_convex.out" ]; then
+	echo "\"Lattice Size\", \"NY\", \"Gamma\", \"Tau\", \"Error\"" > Figure3_convex.out
+	parallel -j4 --colsep '\t' ./Poiseuille/poiseuille_convex --gamma {1} --tau {2} --Ny {3} >> Figure3_convex.out :::: Figure3_convex.pars
+fi;
