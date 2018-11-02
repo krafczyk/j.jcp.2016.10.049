af49dd6126511ae75f5d4c7995352b47aa51e7ab
Matthew Krafczyk <krafczyk.matthew@gmail.com>
2018-11-02 15:29:47 -0500

    Update Figure 4 plot script

--- Figure4.py
+++ Figure4.py
@@ -6,43 +6,40 @@ import math
 plt.figure(4, figsize=(12,4), dpi=320)
 
 ## Load Figure 4 nonconvex data
-#nonconvex_data = []
-#first = True
-#with open("Figure4_nonconvex.out", "r") as nonconvex_datafile:
-#    csvdata_read = csv.reader(nonconvex_datafile, quotechar='"', delimiter=',')
-#    for row in csvdata_read:
-#        if first:
-#            first = False
-#        else:
-#            nonconvex_data.append([float(row[0]), float(row[2]), float(row[3]), float(row[4])])
-#
-## Arrange data by gamma/tau.
-#
-#nonconvex_data_dict = {}
-#
-## Add data
-#for row in nonconvex_data:
-#    gamma = row[1]
-#    if gamma not in nonconvex_data_dict:
-#        nonconvex_data_dict[gamma] = {}
-#    tau = row[2]
-#    if tau not in nonconvex_data_dict[gamma]:
-#        nonconvex_data_dict[gamma][tau] = {'h':[], 'e':[]}
-#    h = row[0]
-#    e = row[3]
-#    if h not in nonconvex_data_dict[gamma][tau]['h']:
-#        nonconvex_data_dict[gamma][tau]['h'].append(h)
-#        nonconvex_data_dict[gamma][tau]['e'].append(e)
-#
-## Sort data
-#for gamma in nonconvex_data_dict:
-#    for tau in nonconvex_data_dict[gamma]:
-#        hlist = nonconvex_data_dict[gamma][tau]['h']
-#        elist = nonconvex_data_dict[gamma][tau]['e']
-#        # From https://stackoverflow.com/questions/9764298/is-it-possible-to-sort-two-listswhich-reference-each-other-in-the-exact-same-w
-#        hsorted, esorted = (list(t) for t in zip(*sorted(zip(hlist, elist))))
-#        nonconvex_data_dict[gamma][tau]['h'] = hsorted
-#        nonconvex_data_dict[gamma][tau]['e'] = esorted
+nonconvex_data = []
+first = True
+with open("Figure4_nonconvex.out", "r") as nonconvex_datafile:
+    csvdata_read = csv.reader(nonconvex_datafile, quotechar='"', delimiter=',')
+    for row in csvdata_read:
+        if first:
+            first = False
+        else:
+            if "nan" not in row[3]:
+                nonconvex_data.append([float(row[0]), float(row[2]), float(row[3])])
+
+# Arrange data by tau.
+
+nonconvex_data_dict = {}
+
+# Add data
+for row in nonconvex_data:
+    tau = row[1]
+    if tau not in nonconvex_data_dict:
+        nonconvex_data_dict[tau] = {'h':[], 'e':[]}
+    h = row[0]
+    e = row[2]
+    if h not in nonconvex_data_dict[tau]['h']:
+        nonconvex_data_dict[tau]['h'].append(h)
+        nonconvex_data_dict[tau]['e'].append(e)
+
+# Sort data
+for tau in nonconvex_data_dict:
+    hlist = nonconvex_data_dict[tau]['h']
+    elist = nonconvex_data_dict[tau]['e']
+    # From https://stackoverflow.com/questions/9764298/is-it-possible-to-sort-two-listswhich-reference-each-other-in-the-exact-same-w
+    hsorted, esorted = (list(t) for t in zip(*sorted(zip(hlist, elist))))
+    nonconvex_data_dict[tau]['h'] = hsorted
+    nonconvex_data_dict[tau]['e'] = esorted
 
 # Load Figure 3 convex data
 convex_data = []
@@ -53,7 +50,8 @@ with open("Figure4_convex.out", "r") as convex_datafile:
         if first:
             first = False
         else:
-            convex_data.append([float(row[0]), float(row[2]), float(row[3])])
+            if row[3] != "-nan":
+                convex_data.append([float(row[0]), float(row[2]), float(row[3])])
 
 # Arrange data by tau.
 
@@ -89,30 +87,24 @@ def make_slope2line(x1, x2, y1):
 plt.rc('text', usetex=True)
 plt.rc('font', family='serif')
 
-slopeline_range = [min(make_log10(convex_data_dict[3.0]['h'])), max(make_log10(convex_data_dict[3.0]['h']))]
+slopeline_range = [min(make_log10(nonconvex_data_dict[2.0]['h'])), max(make_log10(nonconvex_data_dict[2.0]['h']))]
 
-#plt.subplot(121)
-#
-#plt.plot([], color="#FF0000", linestyle="-", marker="x", label="$\\tau=0.505$")
-#plt.plot([], color="#02FF02", linestyle="-", marker="o", markerfacecolor="None", label="$\\tau=0.6$")
-#plt.plot([], color="#0000FF", linestyle="-", marker="v", markerfacecolor="None", label="$\\tau=1$")
-#plt.plot([], color="#000000", linestyle="-", marker="d", markerfacecolor="None", label="$\\tau=2$")
-#plt.plot([], color="#FF75FF", linestyle="-", marker="s", markerfacecolor="None", label="$\\tau=3$")
-#plt.plot([], color="#000000", linestyle="--", label="Slope=2")
-#
-#plt.plot(make_log10(nonconvex_data_dict[0.505]['h']), make_log10(nonconvex_data_dict[0.505]['e']), color="#FF0000", linestyle="-", marker="x")
-#plt.plot(make_log10(nonconvex_data_dict[0.6]['h']), make_log10(nonconvex_data_dict[0.6]['e']), color="#02FF02", linestyle="-", marker="o", markerfacecolor="None")
-#plt.plot(make_log10(nonconvex_data_dict[1.0]['h']), make_log10(nonconvex_data_dict[1.0]['e']), color="#0000FF", linestyle="-", marker="v", markerfacecolor="None")
-#plt.plot(make_log10(nonconvex_data_dict[2.0]['h']), make_log10(nonconvex_data_dict[2.0]['e']), color="#000000", linestyle="-", marker="v", markerfacecolor="None")
-#plt.plot(make_log10(nonconvex_data_dict[3.0]['h']), make_log10(nonconvex_data_dict[3.0]['e']), color="#FF75FF", linestyle="-", marker="v", markerfacecolor="None")
-#slopeline = make_slope2line(slopeline_range[0], slopeline_range[1], -4)
-#plt.plot(slopeline['x'], slopeline['y'], color="#000000", linestyle="--")
-#
-#plt.legend(frameon=False)
-#plt.xlabel(r"$\log_{10}h$")
-#plt.ylabel(r"$\log_{10}E_r$")
-#plt.ylim(-4,-0.7)
-#plt.xlim(-2, -0.9)
+plt.subplot(121)
+
+plt.plot([], color="#0000FF", linestyle="-", marker="v", markerfacecolor="None", label="$\\tau=1$")
+plt.plot([], color="#000000", linestyle="-", marker="d", markerfacecolor="None", label="$\\tau=2$")
+plt.plot([], color="#000000", linestyle="--", label="Slope=2")
+
+plt.plot(make_log10(nonconvex_data_dict[1.0]['h']), make_log10(nonconvex_data_dict[1.0]['e']), color="#0000FF", linestyle="-", marker="v", markerfacecolor="None")
+plt.plot(make_log10(nonconvex_data_dict[2.0]['h']), make_log10(nonconvex_data_dict[2.0]['e']), color="#000000", linestyle="-", marker="v", markerfacecolor="None")
+slopeline = make_slope2line(slopeline_range[0], slopeline_range[1], -4)
+plt.plot(slopeline['x'], slopeline['y'], color="#000000", linestyle="--")
+
+plt.legend(frameon=False)
+plt.xlabel(r"$\log_{10}h$")
+plt.ylabel(r"$\log_{10}E_r$")
+plt.ylim(-5,0)
+plt.xlim(-2.5, -1.5)
 
 plt.subplot(122)
 
