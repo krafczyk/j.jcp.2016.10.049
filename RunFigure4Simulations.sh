067ffa196414ea49c47995f23d14767a09b34970
Matthew Krafczyk <krafczyk.matthew@gmail.com>
2018-11-02 15:32:52 -0500

    Update Simulation script for Figure 4.

--- RunFigure4Simulations.sh
+++ RunFigure4Simulations.sh
@@ -1,11 +1,11 @@
-#echo "Left Figures"
-#if [ ! -e "Figure4_nonconvex.out" ]; then
-#	echo "\"Lattice Size\", \"NY\", \"Tau\", \"Error\"" > Figure4_nonconvex.out
-#	parallel -j4 --colsep '\t' ./bin/taylor_vortex_nonconvex --tau {1} --Ny {2} >> Figure4_nonconvex.out :::: Figure4_nonconvex.pars
-#fi;
+echo "Left Figures"
+if [ ! -e "Figure4_nonconvex.out" ]; then
+	echo "\"Lattice Size\", \"NY\", \"Tau\", \"Error\"" > Figure4_nonconvex.out
+	parallel -j4 --colsep '\t' ./bin/taylor_vortex_nonconvex --tau {1} --Ny {2} >> Figure4_nonconvex.out :::: Figure4_nonconvex.pars
+fi;
 
 echo "Right Figures"
 if [ ! -e "Figure4_convex.out" ]; then
 	echo "\"Lattice Size\", \"NY\", \"Tau\", \"Error\"" > Figure4_convex.out
-	parallel -j2 --colsep '\t' ./bin/taylor_vortex_convex --tau {1} --Ny {2} >> Figure4_convex.out :::: Figure4_convex.pars
+	parallel -j4 --colsep '\t' ./bin/taylor_vortex_convex --tau {1} --Ny {2} >> Figure4_convex.out :::: Figure4_convex.pars
 fi;
