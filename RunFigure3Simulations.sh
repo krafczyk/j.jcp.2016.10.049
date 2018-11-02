03c6760c639ed10f806000a212cc318edd648c57
Matthew Krafczyk <krafczyk.matthew@gmail.com>
2018-11-02 15:35:47 -0500

    Update Run script for experiment 3

--- RunFigure3Simulations.sh
+++ RunFigure3Simulations.sh
@@ -1,11 +1,11 @@
 echo "Left Figures"
 if [ ! -e "Figure3_nonconvex.out" ]; then
 	echo "\"Lattice Size\", \"NY\", \"Gamma\", \"Tau\", \"Error\"" > Figure3_nonconvex.out
-	parallel -j4 --colsep '\t' ./Poiseuille/poiseuille_nonconvex --gamma {1} --tau {2} --Ny {3} >> Figure3_nonconvex.out :::: Figure3_nonconvex.pars
+	parallel -j4 --colsep '\t' ./bin/poiseuille_nonconvex --gamma {1} --tau {2} --Ny {3} >> Figure3_nonconvex.out :::: Figure3_nonconvex.pars
 fi;
 
 echo "Right Figures"
 if [ ! -e "Figure3_convex.out" ]; then
 	echo "\"Lattice Size\", \"NY\", \"Gamma\", \"Tau\", \"Error\"" > Figure3_convex.out
-	parallel -j4 --colsep '\t' ./Poiseuille/poiseuille_convex --gamma {1} --tau {2} --Ny {3} >> Figure3_convex.out :::: Figure3_convex.pars
+	parallel -j4 --colsep '\t' ./bin/poiseuille_convex --gamma {1} --tau {2} --Ny {3} >> Figure3_convex.out :::: Figure3_convex.pars
 fi;
