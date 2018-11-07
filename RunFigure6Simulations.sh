66061cacbbc5ba776a8b461e3dfe19373a7095c8
Matthew Krafczyk <krafczyk.matthew@gmail.com>
2018-11-07 13:59:56 -0600

    Add Figure 6 files

---
+++ RunFigure6Simulations.sh
@@ -0,0 +1,11 @@
+echo "Left Figures"
+if [ ! -e "Figure6_nonconvex.out" ]; then
+	echo "\"Lattice Size\", \"NY\", \"Tau\", \"Error\"" > Figure6_nonconvex.out
+	parallel -j4 --colsep '\t' ./bin/taylor_vortex_nonconvex --tau {1} --Ny {2} >> Figure6_nonconvex.out :::: Figure6_nonconvex.pars
+fi;
+
+echo "Right Figures"
+if [ ! -e "Figure6_convex.out" ]; then
+	echo "\"Lattice Size\", \"NY\", \"Tau\", \"Error\"" > Figure6_convex.out
+	parallel -j4 --colsep '\t' ./bin/taylor_vortex_convex --tau {1} --Ny {2} >> Figure6_convex.out :::: Figure6_convex.pars
+fi;
