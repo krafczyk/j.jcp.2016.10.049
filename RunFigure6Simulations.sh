c0e90c630209ed8aa59cbc837dfb99a78f4d8741
Matthew Krafczyk <krafczyk.matthew@gmail.com>
2018-11-09 14:53:14 -0600

    Fix Figure 6 script

--- RunFigure6Simulations.sh
+++ RunFigure6Simulations.sh
@@ -5,12 +5,12 @@ fi;
 
 echo "Left Figures"
 if [ ! -e "Figure6_nonconvex.out" ]; then
-	echo "\"Lattice Size\", \"NY\", \"Tau\", \"Error\"" > Figure6_nonconvex.out
-	parallel -j${jobs} --colsep '\t' ./bin/taylor_vortex_nonconvex --tau {1} --Ny {2} >> Figure6_nonconvex.out :::: Figure6_nonconvex.pars
+	echo "\"Lattice Size\", \"NY\", \"Beta\", \"Tau\", \"Error\"" > Figure6_nonconvex.out
+	parallel -j${jobs} --colsep '\t' ./bin/Couette_circle_nonconvex --beta 0.5 --tau {1} --Ny {2} >> Figure6_nonconvex.out :::: Figure6_nonconvex.pars
 fi;
 
 echo "Right Figures"
 if [ ! -e "Figure6_convex.out" ]; then
-	echo "\"Lattice Size\", \"NY\", \"Tau\", \"Error\"" > Figure6_convex.out
-	parallel -j${jobs} --colsep '\t' ./bin/taylor_vortex_convex --tau {1} --Ny {2} >> Figure6_convex.out :::: Figure6_convex.pars
+	echo "\"Lattice Size\", \"NY\", \"Beta\", \"Tau\", \"Error\"" > Figure6_convex.out
+	parallel -j${jobs} --colsep '\t' ./bin/Couette_circle_convex --beta 0.5 --tau {1} --Ny {2} >> Figure6_convex.out :::: Figure6_convex.pars
 fi;
