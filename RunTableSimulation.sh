b545a1c6ecf0a065c0bf9a184bd5cf05f3a4c5f2
Matthew Krafczyk <krafczyk.matthew@gmail.com>
2018-12-11 16:50:48 -0600

    Reduce precision of Table 1 simulations by factor of 10.

--- RunTableSimulation.sh
+++ RunTableSimulation.sh
@@ -5,4 +5,5 @@ if [ -e "${output_file}" ]; then
 	rm ${output_file};
 fi;
 echo "Scanning parameters for the $1 scheme with gamma $2."
-parallel -j8 './bin/poiseuille_{1}_stability --gamma {2} --tau $(expr {3} / 10000).$(expr {3} % 10000 | xargs printf "%04d") --Ny 41' >> ${output_file} ::: $1 ::: $2 ::: {5001..30000}
+./bin/poiseuille_$1_stability --gamma $2 --tau 0.5001 --Ny 41 >> ${output_file}
+parallel -j8 './bin/poiseuille_{1}_stability --gamma {2} --tau $(expr {3} / 1000).$(expr {3} % 1000 | xargs printf "%03d") --Ny 41' >> ${output_file} ::: $1 ::: $2 ::: {501..3000}
