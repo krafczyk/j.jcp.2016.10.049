18cf6a10bb9e9ae0f43db237ab1f2374c360df5d
Matthew Krafczyk <krafczyk.matthew@gmail.com>
2018-12-18 10:22:00 -0600

    Add run.sh script

---
+++ run.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+set -e
+
+# Build binaries
+build.sh
+
+# Run Computational Experiments
+bash RunFigure3Simulations.sh
+bash RunFigure4Simulations.sh
+bash RunFigure5Simulations.sh
+bash RunFigure6Simulations.sh
+bash RunTable1Simulations.sh
+
+# Run Visualization Scripts
+bash Figure3.py
+bash Figure4.py
+bash Figure5.py
+bash Figure6.py
+bash Table1.py
